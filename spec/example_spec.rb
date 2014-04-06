e4ea60c2bce9a2f5307052597725ca18f8004179
David Grayson <davidegrayson@gmail.com>
2014-04-06 11:02:15 -0700

    Fixed trailing whitespace.

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -15,13 +15,13 @@ describe 'examples in README.md' do
     puts 'public key: '
     puts '  x: %#x' % public_key.x
     puts '  y: %#x' % public_key.y
-    
+
     # Encoding a public key as a binary string
     public_key_string = ECDSA::Format::PointOctetString.encode(public_key, compression: true)
 
     # Decoding a public key from a binary string
     public_key = ECDSA::Format::PointOctetString.decode(public_key_string, group)
-    
+
     # Signing a message
     require 'digest/sha2'
     message = 'ECDSA is cool.'
@@ -34,13 +34,13 @@ describe 'examples in README.md' do
     puts 'signature: '
     puts '  r: %#x' % signature.r
     puts '  s: %#x' % signature.s
-    
+
     # Encoding a signature as a DER string
     signature_der_string = ECDSA::Format::SignatureDerString.encode(signature)
 
     # Decoding a signature from a DER string
     signature = ECDSA::Format::SignatureDerString.decode(signature_der_string)
-    
+
     # Verifying a signature
     valid = ECDSA.valid_signature?(public_key, digest, signature)
     puts "valid: #{valid}"
